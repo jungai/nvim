eab7cc09d80dc0674b0bc33294ec8fb54b88b89b
jungai <juworaphol125@gmail.com>
2023-02-06 14:31:37 +0700

    fix wrong keymap

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -104,9 +104,9 @@ return {
 		keys = {
 			{ "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
 			{ "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-			{ "<leader_>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-			{ "<leader_>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-			{ "<leader_>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+			{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+			{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
 		},
 	},
 
@@ -115,8 +115,8 @@ return {
 		cmd = { "TroubleToggle", "Trouble" },
 		opts = { use_diagnostic_signs = true },
 		keys = {
-			{ "<leader_>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-			{ "<leader_>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+			{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
 		},
 	},
 
