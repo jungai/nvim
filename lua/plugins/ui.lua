8f938f8be35f51a8fe2740173db4e9fbb19631d2
jungai <juworaphol125@gmail.com>
2023-02-15 11:47:39 +0700

    remove indent blankline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,18 +33,6 @@ return {
 	},
 
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "BufReadPost",
-		opts = {
-			-- char = "▏",
-			char = "│",
-			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
-		},
-	},
-
-	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		dependencies = { { "MunifTanjim/nui.nvim" } },
