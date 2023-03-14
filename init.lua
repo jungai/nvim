3b67fa39b6a6c8524a7b7f6c103dcc5677392fac
jungai <juworaphol125@gmail.com>
2023-03-14 23:34:11 +0700

    vscode theme

--- init.lua
+++ init.lua
@@ -1,37 +1,37 @@
-require("config/options")
-require("config/keymaps")
+require "config/options"
+require "config/keymaps"
 
 vim.g.mapleader = " "
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	-- {
-	-- 	"Mofiqul/vscode.nvim",
-	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	-- 	priority = 1000, -- make sure to load this before all the other start plugins
-	-- 	config = true,
-	-- },
-	{
-		"svrana/neosolarized.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = true,
-		dependencies = { "tjdevries/colorbuddy.nvim" },
-		opts = {
-			comment_italics = true,
-			background_set = false,
-		},
-	},
-	{ import = "plugins" },
-})
+require("lazy").setup {
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = true,
+  },
+  -- {
+  --   "svrana/neosolarized.nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = true,
+  --   dependencies = { "tjdevries/colorbuddy.nvim" },
+  --   opts = {
+  --     comment_italics = true,
+  --     background_set = false,
+  --   },
+  -- },
+  { import = "plugins" },
+}
