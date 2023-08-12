5689e1cf6024426074878d044e5c388fbe6102ac
jungai <juworaphol125@gmail.com>
2023-08-12 21:27:02 +0700

    add indent blankline, flash

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -326,4 +326,30 @@ return {
     "chiedo/vim-case-convert",
     event = "VeryLazy",
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufRead",
+    config = function()
+      vim.opt.list = true
+      vim.opt.listchars:append "eol:↴"
+
+      require("indent_blankline").setup {
+        show_end_of_line = true,
+      }
+    end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
 }
