f889b1a381866dc9d462efda21b76f235eef32d8
jungai <juworaphol125@gmail.com>
2023-09-03 14:22:01 +0700

    add lazygit.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -338,4 +338,16 @@ return {
     "sindrets/diffview.nvim",
     event = "VeryLazy",
   },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+
+    cmd = "LazyGit",
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open Lazygit", remap = true },
+    },
+  },
 }
