c79610954d9a45a843d800050c439f5d9ae862b3
jungai <juworaphol125@gmail.com>
2023-10-23 12:12:15 +0700

    bump deps

--- init.lua
+++ init.lua
@@ -17,17 +17,16 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "EdenEast/nightfox.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("rose-pine").setup {
-        disable_italics = true,
-        dim_nc_background = true,
-        disable_background = true,
+      require("nightfox").setup {
+        options = {
+          transparent = true,
+        },
       }
-      vim.cmd "colorscheme rose-pine"
+      vim.cmd "colorscheme duskfox"
     end,
   },
   { import = "plugins" },
