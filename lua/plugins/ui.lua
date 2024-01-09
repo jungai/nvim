f9d1bb7f70f018811e9feb24a529d7f762258288
jungai <juworaphol125@gmail.com>
2024-01-09 08:56:05 +0700

    update packages

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -83,23 +83,40 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-      mode = "showmode",
-      messages = {
-        enabled = true, -- enables the Noice messages UI
-        view = false, -- default view for messages
-        view_error = false, -- view for errors
-        view_warn = false, -- view for warnings
-        view_history = false, -- view for :messages
-        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      -- mode = "showmode",
+      -- -- messages = {
+      -- --   enabled = true, -- enables the Noice messages UI
+      -- --   view = true, -- default view for messages
+      -- --   view_error = true, -- view for errors
+      -- --   view_warn = true, -- view for warnings
+      -- --   view_history = false, -- view for :messages
+      -- --   view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      -- -- },
+      -- -- add any options here
+      -- presets = {
+      --   lsp_doc_border = true,
+      -- },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
       },
-      -- add any options here
+      -- you can enable a preset for easier configuration
       presets = {
-        lsp_doc_border = true,
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
       },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
     },
   },
 }
