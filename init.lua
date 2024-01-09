fb23500826ead28a5dbf7018a79fc4083a99e4a8
jungai <juworaphol125@gmail.com>
2024-01-10 00:10:58 +0700

    back to tokyonight

--- init.lua
+++ init.lua
@@ -54,17 +54,17 @@ require("lazy").setup {
   --     transparent_background = true,
   --   },
   -- },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      variant = "moon", -- auto, main, moon, or dawn
-      dark_variant = "moon", -- main, moon, or dawn
-      styles = {
-        transparency = true,
-      },
-    },
-  },
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   opts = {
+  --     variant = "moon", -- auto, main, moon, or dawn
+  --     dark_variant = "moon", -- main, moon, or dawn
+  --     styles = {
+  --       transparency = true,
+  --     },
+  --   },
+  -- },
   -- {
   --   "oxfist/night-owl.nvim",
   --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -74,7 +74,43 @@ require("lazy").setup {
   --     vim.cmd.colorscheme "night-owl"
   --   end,
   -- },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local bg = "#011628"
+      local bg_dark = "#011423"
+      local bg_highlight = "#143652"
+      local bg_search = "#0A64AC"
+      local bg_visual = "#275378"
+      local fg = "#CBE0F0"
+      local fg_dark = "#B4D0E9"
+      local fg_gutter = "#627E97"
+      local border = "#547998"
+
+      require("tokyonight").setup {
+        style = "night",
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = bg_dark
+          colors.bg_float = bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = bg_dark
+          colors.bg_statusline = bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end,
+      }
+    end,
+  },
   { import = "plugins" },
 }
 
-vim.cmd [[colorscheme rose-pine]]
+vim.cmd [[colorscheme tokyonight]]
