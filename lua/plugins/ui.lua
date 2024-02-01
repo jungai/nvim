ad802f6b558e0e2ef03a45c74a82f39bb58f765d
jungai <juworaphol125@gmail.com>
2024-02-01 14:13:35 +0700

    config notify width

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,26 +1,19 @@
 return {
-  -- {
-  --   "rcarriga/nvim-notify",
-  --   keys = {
-  --     {
-  --       "<leader>un",
-  --       function()
-  --         require("notify").dismiss { silent = true, pending = true }
-  --       end,
-  --       desc = "Delete all Notifications",
-  --     },
-  --   },
-  --   opts = {
-  --     timeout = 3000,
-  --     max_height = function()
-  --       return math.floor(vim.o.lines * 0.75)
-  --     end,
-  --     max_width = function()
-  --       return math.floor(vim.o.columns * 0.75)
-  --     end,
-  --   },
-  -- },
-  --
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    opts = {
+      timeout = 500,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.5)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.5)
+      end,
+      render = "compact",
+    },
+  },
+
   -- {
   --   "akinsho/bufferline.nvim",
   --   event = "VeryLazy",
