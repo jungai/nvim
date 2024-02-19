5edecf984a9712f45e757af3358e096d1858e3fa
jungai <juworaphol125@gmail.com>
2024-02-19 11:17:53 +0700

    try ayu

--- init.lua
+++ init.lua
@@ -36,7 +36,7 @@ require("lazy").setup {
   --   priority = 1000,
   --   config = function()
   --     vim.g.nightflyCursorColor = true
-  --     vim.g.nightflyTransparent = true
+  --     vim.g.nightflyTransparent = false
   --     vim.g.nightflyVirtualTextColor = true
   --     vim.cmd [[colorscheme nightfly]]
   --   end,
@@ -54,13 +54,33 @@ require("lazy").setup {
   --     transparent_background = true,
   --   },
   -- },
+  {
+    "Shatur/neovim-ayu",
+    name = "ayu",
+    priority = 1000,
+    opts = {
+      overrides = {
+        Normal = { bg = "None" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        WhichKeyFloat = { bg = "None" },
+        VertSplit = { bg = "None" },
+      },
+    },
+  },
   -- {
   --   "rose-pine/neovim",
   --   name = "rose-pine",
   --   opts = {
-  --     variant = "moon", -- auto, main, moon, or dawn
-  --     dark_variant = "moon", -- main, moon, or dawn
+  --     -- variant = "moon", -- auto, main, moon, or dawn
+  --     -- dark_variant = "moon", -- main, moon, or dawn
   --     styles = {
+  --       bold = true,
+  --       italic = false,
   --       transparency = true,
   --     },
   --   },
@@ -74,44 +94,44 @@ require("lazy").setup {
   --     vim.cmd.colorscheme "night-owl"
   --   end,
   -- },
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
-
-      require("tokyonight").setup {
-        style = "night",
-        -- transparent = true,
-        on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = bg_dark
-          colors.bg_float = bg_dark
-          colors.bg_highlight = bg_highlight
-          colors.bg_popup = bg_dark
-          colors.bg_search = bg_search
-          colors.bg_sidebar = bg_dark
-          colors.bg_statusline = bg_dark
-          colors.bg_visual = bg_visual
-          colors.border = border
-          colors.fg = fg
-          colors.fg_dark = fg_dark
-          colors.fg_float = fg
-          colors.fg_gutter = fg_gutter
-          colors.fg_sidebar = fg_dark
-        end,
-      }
-    end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     local bg = "#011628"
+  --     local bg_dark = "#011423"
+  --     local bg_highlight = "#143652"
+  --     local bg_search = "#0A64AC"
+  --     local bg_visual = "#275378"
+  --     local fg = "#CBE0F0"
+  --     local fg_dark = "#B4D0E9"
+  --     local fg_gutter = "#627E97"
+  --     local border = "#547998"
+  --
+  --     require("tokyonight").setup {
+  --       style = "night",
+  --       -- transparent = true,
+  --       on_colors = function(colors)
+  --         colors.bg = bg
+  --         colors.bg_dark = bg_dark
+  --         colors.bg_float = bg_dark
+  --         colors.bg_highlight = bg_highlight
+  --         colors.bg_popup = bg_dark
+  --         colors.bg_search = bg_search
+  --         colors.bg_sidebar = bg_dark
+  --         colors.bg_statusline = bg_dark
+  --         colors.bg_visual = bg_visual
+  --         colors.border = border
+  --         colors.fg = fg
+  --         colors.fg_dark = fg_dark
+  --         colors.fg_float = fg
+  --         colors.fg_gutter = fg_gutter
+  --         colors.fg_sidebar = fg_dark
+  --       end,
+  --     }
+  --   end,
+  -- },
   { import = "plugins" },
 }
 
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme ayu-mirage]]
