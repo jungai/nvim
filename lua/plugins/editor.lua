5482454445229bd276ca6604505e3909cd8c8407
jungai <juworaphol125@gmail.com>
2024-04-05 14:16:18 +0700

    migrate to mini.indent & update change gitsign symbol

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -24,46 +24,54 @@ return {
     },
   },
   {
-    -- indent guides for Neovim
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowTeal",
-        "RainbowGreen",
-        "RainbowOrange",
-        "RainbowCyan",
-      }
-
-      local hooks = require "ibl.hooks"
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ea6962" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#fabd2f" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#458588" })
-        vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#8ec07c" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#b8bb26" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#e78a4e" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#83a598" })
-      end)
-
-      local ibl = require "ibl"
-      ibl.setup {
-        indent = {
-          char = "│",
-          tab_char = "│",
-          highlight = highlight,
-        },
-        scope = {
-          enabled = false,
-        },
-        vim.opt.listchars:append "space: ",
-      }
-    end,
+    "echasnovski/mini.indentscope",
+    version = "*",
+    event = "BufReadPre",
+    opts = {
+      symbol = "│",
+    },
   },
+
+  -- {
+  --   -- indent guides for Neovim
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   config = function()
+  --     -- local highlight = {
+  --     --   "RainbowRed",
+  --     --   "RainbowYellow",
+  --     --   "RainbowBlue",
+  --     --   "RainbowTeal",
+  --     --   "RainbowGreen",
+  --     --   "RainbowOrange",
+  --     --   "RainbowCyan",
+  --     -- }
+  --     --
+  --     -- local hooks = require "ibl.hooks"
+  --     -- -- create the highlight groups in the highlight setup hook, so they are reset
+  --     -- -- every time the colorscheme changes
+  --     -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  --     --   vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ea6962" })
+  --     --   vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#fabd2f" })
+  --     --   vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#458588" })
+  --     --   vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#8ec07c" })
+  --     --   vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#b8bb26" })
+  --     --   vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#e78a4e" })
+  --     --   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#83a598" })
+  --     -- end)
+  --
+  --     local ibl = require "ibl"
+  --     ibl.setup {
+  --       indent = {
+  --         char = "│",
+  --         tab_char = "│",
+  --       },
+  --       scope = {
+  --         enabled = true,
+  --       },
+  --       vim.opt.listchars:append "space: ",
+  --     }
+  --   end,
+  -- },
   -- {
   --   "echasnovski/mini.indentscope",
   --   version = false, -- wait till new 0.7.0 release to put it back on semver
@@ -195,7 +203,7 @@ return {
       current_line_blame = true,
       signs = {
         add = { text = "+" },
-        change = { text = "c" },
+        change = { text = "M" },
         delete = { text = "-" },
         topdelete = { text = "‾" },
         changedelete = { text = "~" },
