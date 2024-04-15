ff3783b9e1d8539a18de51c1403616f34670a177
jungai <juworaphol125@gmail.com>
2024-04-15 11:50:27 +0700

    catppuccin macchiato

--- init.lua
+++ init.lua
@@ -94,56 +94,56 @@ require("lazy").setup {
   --     vim.cmd.colorscheme "night-owl"
   --   end,
   -- },
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
-
-      require("tokyonight").setup {
-        style = "moon",
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
-        },
-        -- transparent = true,
-        -- on_colors = function(colors)
-        --   colors.bg = bg
-        --   colors.bg_dark = bg_dark
-        --   colors.bg_float = bg_dark
-        --   colors.bg_highlight = bg_highlight
-        --   colors.bg_popup = bg_dark
-        --   colors.bg_search = bg_search
-        --   colors.bg_sidebar = bg_dark
-        --   colors.bg_statusline = bg_dark
-        --   colors.bg_visual = bg_visual
-        --   colors.border = border
-        --   colors.fg = fg
-        --   colors.fg_dark = fg_dark
-        --   colors.fg_float = fg
-        --   colors.fg_gutter = fg_gutter
-        --   colors.fg_sidebar = fg_dark
-        -- end,
-      }
-    end,
-  },
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, event = "VimEnter" },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     local bg = "#011628"
+  --     local bg_dark = "#011423"
+  --     local bg_highlight = "#143652"
+  --     local bg_search = "#0A64AC"
+  --     local bg_visual = "#275378"
+  --     local fg = "#CBE0F0"
+  --     local fg_dark = "#B4D0E9"
+  --     local fg_gutter = "#627E97"
+  --     local border = "#547998"
+  --
+  --     require("tokyonight").setup {
+  --       style = "moon",
+  --       styles = {
+  --         -- Style to be applied to different syntax groups
+  --         -- Value is any valid attr-list value for `:help nvim_set_hl`
+  --         comments = { italic = false },
+  --         keywords = { italic = false },
+  --         functions = {},
+  --         variables = {},
+  --         -- Background styles. Can be "dark", "transparent" or "normal"
+  --         sidebars = "dark", -- style for sidebars, see below
+  --         floats = "dark", -- style for floating windows
+  --       },
+  --       -- transparent = true,
+  --       -- on_colors = function(colors)
+  --       --   colors.bg = bg
+  --       --   colors.bg_dark = bg_dark
+  --       --   colors.bg_float = bg_dark
+  --       --   colors.bg_highlight = bg_highlight
+  --       --   colors.bg_popup = bg_dark
+  --       --   colors.bg_search = bg_search
+  --       --   colors.bg_sidebar = bg_dark
+  --       --   colors.bg_statusline = bg_dark
+  --       --   colors.bg_visual = bg_visual
+  --       --   colors.border = border
+  --       --   colors.fg = fg
+  --       --   colors.fg_dark = fg_dark
+  --       --   colors.fg_float = fg
+  --       --   colors.fg_gutter = fg_gutter
+  --       --   colors.fg_sidebar = fg_dark
+  --       -- end,
+  --     }
+  --   end,
+  -- },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, event = "VimEnter" },
   { import = "plugins" },
 }
 
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme catppuccin-macchiato]]
