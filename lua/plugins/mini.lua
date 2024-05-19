e3ba767f13b81ffcc4452571c1ed4cce30f9757c
jungai <juworaphol125@gmail.com>
2024-05-19 12:46:10 +0700

    remove duplicate packages

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,13 +7,6 @@ return {
     end,
   },
   {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    config = function(_, opts)
-      require("mini.pairs").setup(opts)
-    end,
-  },
-  {
     "echasnovski/mini.indentscope",
     version = "*",
     event = "BufReadPre",
