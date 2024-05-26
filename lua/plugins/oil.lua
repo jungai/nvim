30a46bbfa8817b3e621ad98d517a7af5e5738e4c
jungai <juworaphol125@gmail.com>
2024-05-26 21:01:51 +0700

    add float in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/oil.nvim",
     cmd = "Oil",
     keys = {
-      { "-", "<cmd>Oil<cr>", mode = "n", desc = "Open parent directory" },
+      { "-", "<cmd>Oil --float<cr>", mode = "n", desc = "Open parent directory" },
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -11,6 +11,7 @@ return {
       view_options = {
         show_hidden = true,
       },
+      -- Configuration for the floating window in oil.open_float
       float = {
         -- Padding around the floating window
         padding = 2,
@@ -25,6 +26,53 @@ return {
         override = function(conf)
           return conf
         end,
+      },
+      -- Configuration for the actions floating preview window
+      preview = {
+        -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_width and max_width can be a single value or a list of mixed integer/float types.
+        -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+        max_width = 0.9,
+        -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+        min_width = { 40, 0.4 },
+        -- optionally define an integer/float for the exact width of the preview window
+        width = nil,
+        -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_height and max_height can be a single value or a list of mixed integer/float types.
+        -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+        max_height = 0.9,
+        -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+        min_height = { 5, 0.1 },
+        -- optionally define an integer/float for the exact height of the preview window
+        height = nil,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+        -- Whether the preview window is automatically updated when the cursor is moved
+        update_on_cursor_moved = true,
+      },
+      -- Configuration for the floating progress window
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = "rounded",
+        minimized_border = "none",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      -- Configuration for the floating SSH window
+      ssh = {
+        border = "rounded",
+      },
+      -- Configuration for the floating keymaps help window
+      keymaps_help = {
+        border = "rounded",
       },
     },
   },
