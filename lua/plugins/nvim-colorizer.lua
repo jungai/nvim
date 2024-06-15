1470ab1e5c723b661289bd10ea59b1641fc349cf
jungai <juworaphol125@gmail.com>
2024-06-15 22:00:52 +0700

    disabled nvim-colorizer

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "NvChad/nvim-colorizer.lua",
-    name = "colorizer",
-    event = "BufReadPre",
-    config = function()
-      require("colorizer").attach_to_buffer(0, { mode = "background", css = true, tailwind = "both" })
-    end,
-  },
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   name = "colorizer",
+  --   event = "BufReadPre",
+  --   config = function()
+  --     require("colorizer").attach_to_buffer(0, { mode = "background", css = true, tailwind = "both" })
+  --   end,
+  -- },
   {
     "js-everts/cmp-tailwind-colors",
     -- optionally, override the default options:
