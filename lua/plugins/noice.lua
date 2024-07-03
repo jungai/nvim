7a7c077e8ab0e99a08bdff002d433d38deca181f
jungai <juworaphol125@gmail.com>
2024-07-03 12:30:01 +0700

    disabled nvim notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -91,7 +91,7 @@ return {
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+      -- "rcarriga/nvim-notify",
     },
   },
 }
