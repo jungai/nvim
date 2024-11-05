9ed8108a375870e117232836e376c12fe9094faa
jungai <juworaphol125@gmail.com>
2024-11-05 12:45:31 +0700

    use mini comment instead of numToStr/comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,24 @@
 return {
+  -- {
+  --   "numToStr/Comment.nvim",
+  --   keys = { "gcc" },
+  --   config = function()
+  --     require("Comment").setup {
+  --       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  --     }
+  --   end,
+  -- },
   {
-    "numToStr/Comment.nvim",
+    "echasnovski/mini.comment",
+    version = "*",
     keys = { "gcc" },
     config = function()
-      require("Comment").setup {
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      require("mini.comment").setup {
+        options = {
+          custom_commentstring = function()
+            return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          end,
+        },
       }
     end,
   },
